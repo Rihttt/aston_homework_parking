b3bd3811d06c3368e253a274b3654f1bca119988
Kirill Shurygin <Kirill Shurygin>
2025-01-24 09:50:20 +0300

    add pom.xml and main app

---
+++ src/main/java/ru/aston/ParkingApp.java
@@ -0,0 +1,13 @@
+package ru.aston;
+
+/**
+ * Hello world!
+ *
+ */
+public class ParkingApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
